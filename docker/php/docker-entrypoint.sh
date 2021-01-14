f2992a74a14cacd8898881e2cad8f8f232cf87d1
Emanuele Panzeri <thePanz@users.noreply.github.com>
2021-01-14 19:08:17 +0100

    fix: properly check for DB environment
    Make sure the `DATABASE_URL` is checked without comments :)

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -29,7 +29,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
 
-	if grep -q DATABASE_URL= .env; then
+	if grep -q ^DATABASE_URL= .env; then
 		echo "Waiting for db to be ready..."
 		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
 		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || bin/console doctrine:query:sql "SELECT 1" >/dev/null 2>&1; do
