cb77bd16f39a0ae8230c1efc8a811416e966af3d
Guillaume Ponty <34276317+gponty@users.noreply.github.com>
2021-03-25 17:20:06 +0100

    feat: Try to create empty database at startup, in order to avoid the error message : "The database is not up or not reachable" if database doesnt exist. (#119)

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -30,6 +30,8 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	fi
 
 	if grep -q ^DATABASE_URL= .env; then
+		echo "Try to create database"
+		bin/console doctrine:database:create --if-not-exists --no-interaction
 		echo "Waiting for db to be ready..."
 		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
 		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(bin/console doctrine:query:sql "SELECT 1" 2>&1); do
