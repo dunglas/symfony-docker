c317e9c89efa08b4b8d1173a3de88841985db50f
Ronald Rotteveel <ronald.rotteveel@rebelslab.com>
2023-01-26 17:14:13 +0100

    Update docker-entrypoint.sh
    On my machine the php instance was not coming up[ because it was searching for the database. Finally I logged in to the terminal of the image and there I was able to execute the command 'php bin/console dbal:run-sql "SELECT 1"'. Hence, I believe php is missing from the test in the 'until' on line 40.

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -37,7 +37,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 		echo "Waiting for db to be ready..."
 		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
-		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(bin/console dbal:run-sql "SELECT 1" 2>&1); do
+		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql "SELECT 1" 2>&1); do
 			if [ $? -eq 255 ]; then
 				# If the Doctrine command exits with 255, an unrecoverable error occurred
 				ATTEMPTS_LEFT_TO_REACH_DATABASE=0
