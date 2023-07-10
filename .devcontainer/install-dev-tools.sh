e1204ae6a919d92364fbb31fd684860a261dbd74
ili101 <zoby101@gmail.com>
2023-07-10 17:24:33 +0000

    symfony-vscode Extension fix PHP path PowerShell install example

---
+++ .devcontainer/install-dev-tools.sh
@@ -0,0 +1,37 @@
+## Install PowerShell example ##
+# install the requirements
+apk add --no-cache \
+    ca-certificates \
+    less \
+    ncurses-terminfo-base \
+    krb5-libs \
+    libgcc \
+    libintl \
+    libssl1.1 \
+    libstdc++ \
+    tzdata \
+    userspace-rcu \
+    zlib \
+    icu-libs \
+    curl
+
+apk -X https://dl-cdn.alpinelinux.org/alpine/edge/main add --no-cache \
+    lttng-ust
+
+# Download the powershell '.tar.gz' archive
+curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.3.5/powershell-7.3.5-linux-alpine-x64.tar.gz -o /tmp/powershell.tar.gz
+
+# Create the target folder where powershell will be placed
+mkdir -p /opt/microsoft/powershell/7
+
+# Expand powershell to the target folder
+tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+
+# Set execute permissions
+chmod +x /opt/microsoft/powershell/7/pwsh
+
+# Create the symbolic link that points to pwsh
+ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+
+# Start PowerShell
+# pwsh
