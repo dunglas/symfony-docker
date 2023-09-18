20eaccc4d327a74ad3dd26315cf9ebcbcc070592
Kévin Dunglas <kevin@dunglas.fr>
2023-09-18 22:03:49 +0200

    feat: better healthcheck and prevent useless composer install calls

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -27,7 +27,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		fi
 	fi
 
-	if [ "$APP_ENV" != 'prod' ]; then
+	if [ ! -d 'vendor/' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
 
