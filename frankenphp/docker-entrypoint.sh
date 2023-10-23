d6084697f0204d86423d0e8e57fd08ddf9e2a366
Borislav Kosun <kosun.b@yandex.com>
2023-10-23 13:55:43 +0300

    fix: install Composer dependencies if vendor directory is empty

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -22,7 +22,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		fi
 	fi
 
-	if [ ! -d 'vendor/' ]; then
+	if [ -z "$(ls -A 'vendor/')" ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
 
