1aaa8561f6b7707feb2cdae95d412301aa4a88b5
Florian <fouteox@gmail.com>
2024-06-14 18:38:20 +0200

    adjust script for ignore installation and script "à certains endroits"

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -6,16 +6,20 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	# After the installation, the following block can be deleted
 	if [ ! -f composer.json ]; then
 		rm -Rf tmp/
-		composer create-project "laravel/laravel $LARAVEL_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install
+		composer create-project "laravel/laravel $LARAVEL_VERSION" tmp --stability="$STABILITY" --remove-vcs --no-scripts --prefer-dist --no-progress --no-interaction --no-install
 
 		cd tmp
 		cp -Rp . ..
 		cd -
 		rm -Rf tmp/
+
+		composer require "php:>=$PHP_VERSION" --no-scripts --no-install
 	fi
 
     if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
+        composer run-script post-root-package-install
         composer install --prefer-dist --no-progress --no-interaction
+        composer run-script post-create-project-cmd
     fi
 
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX storage
