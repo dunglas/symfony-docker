13e0483847eb7fcba371c8ab0a4ec1e89ff2b54b
Florian <fouteox@gmail.com>
2024-06-14 17:42:06 +0200

    move install

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -6,13 +6,17 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	# After the installation, the following block can be deleted
 	if [ ! -f composer.json ]; then
 		rm -Rf tmp/
-		composer create-project "laravel/laravel $LARAVEL_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction
+		composer create-project "laravel/laravel $LARAVEL_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install
 
 		cd tmp
 		cp -Rp . ..
 		cd -
 		rm -Rf tmp/
 	fi
+
+    if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
+        composer install --prefer-dist --no-progress --no-interaction
+    fi
 
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX storage
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX storage
