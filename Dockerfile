ec18cd9ac1c14832a4531739716e7e701b8f0816
Steffen <s.heitzsch@leuchtturm.com>
2024-09-04 14:12:36 +0200

    Create PDF file

--- Dockerfile
+++ Dockerfile
@@ -22,6 +22,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 	file \
 	gettext \
 	git \
+	wkhtmltopdf \
 	&& rm -rf /var/lib/apt/lists/*
 
 RUN set -eux; \
--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
