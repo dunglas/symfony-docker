5021aee3f858d3e9ba528aff686ece394912c539
Thomas C. <95548437+tcoch@users.noreply.github.com>
2024-11-26 17:39:56 +0100

    Simpler message
    Works for me ;)
    
    Co-authored-by: Kévin Dunglas <kevin@dunglas.fr>

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -56,7 +56,7 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
-	echo 'Entrypoint has finished its work !'
+	echo 'PHP app ready!'
 fi
 
 exec docker-php-entrypoint "$@"
