adaa1f20cf3cf44c82b7b0a2a9ad17b459f0cf58
Thomas C. <95548437+tcoch@users.noreply.github.com>
2024-11-27 15:24:19 +0100

    feat: print message when docker-entrypoint finishes (#703)
    Co-authored-by: Kévin Dunglas <kevin@dunglas.fr>

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -55,6 +55,8 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+
+	echo 'PHP app ready!'
 fi
 
 exec docker-php-entrypoint "$@"
